1645e32796b0d26dcdc743a95acca938c1661b6d
Lavish-Pundir <your-new-lavishpundir90@gmail.com>
2026-04-20 16:59:04 +0530

    core package add

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import userRouter from './routers/user.router.js';
+import cors from 'cors'
 
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
